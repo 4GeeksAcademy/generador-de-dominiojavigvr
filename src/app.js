aa8ed851d7decbda679d7f6dea557418c017ecc9
javigvr <jsanchezgav@gmail.com>
2025-01-15 11:29:09 +0000

    generador de dominios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extensions = [".com", ".net", ".us", ".io"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let domains = [];
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    //
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extensions.length; l++) {
+        domains.push(pronoun[i] + adj[j] + noun[k] + extensions[l]);
+      }
+    }
+  }
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+console.log(domains);
